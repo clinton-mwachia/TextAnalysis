cdbdea2141fa03bc1029e797b7e7f13982895974
clinton moshe <clintonmwachia9@gmail.com>
2019-11-20 16:40:03 +0300

    gutenberg

--- gutenbergr.R
+++ gutenbergr.R
@@ -41,3 +41,22 @@ frequency = bind_rows(mutate(tidy_bronte, author = 'Bronte Sisters'),
   spread(author, proportion) %>% 
   gather(author, proportion, `Bronte Sisters`: `H.G. Wells`)
 frequency
+
+# Plotting the frequensies
+library(scales)
+library(ggplot2)
+
+ggplot(frequency, aes(x=proportion, y=`Jane Austen`,
+                      color=abs(`Jane Austen` - proportion))) +  
+  geom_abline(color='gray', lty=2) +
+  geom_jitter(alpha=0.1, size=2.5, width = 0.3, height = 1.3) + 
+  geom_text(aes(label = word), check_overlap = TRUE, vjust=1.5) + 
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  scale_color_gradient(limits=c(0,0.001),
+                       low='darkslategray4',high = 'gray75') +
+facet_wrap(~author, ncol = 2) +
+  theme(legend.position = 'none') +
+  labs(y = "Jane Austen", x = NULL)
+  
+              
